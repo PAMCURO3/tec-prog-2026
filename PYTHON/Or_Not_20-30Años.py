b6f16d70b33c33b1427e2665b6f74df5f6416a77
Pamela Cuello <pam.rodriguez3@gmail.com>
2026-05-04 19:50:09 -0300

    rango de 20 30 años operador or-not

--- PYTHON/Or_Not_20-30Años.py
+++ PYTHON/Or_Not_20-30Años.py
@@ -1,7 +1,7 @@
 edad = int(input('Digite su edad: '))
 veinte = edad >= 20 and edad < 30
 print(veinte)
-treinta = edad >= 30 and edad < 40
+treinta = edad <= 30 and edad < 40
 print(treinta)
 
 if veinte or treinta:
