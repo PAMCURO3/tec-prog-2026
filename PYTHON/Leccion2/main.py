ae8db0434edc70057a2be74cb0576fe79dfd30fa
Pamela Cuello <pam.rodriguez3@gmail.com>
2026-05-11 21:34:01 -0300

    Leccion2 programación

---
+++ PYTHON/Leccion2/main.py
@@ -0,0 +1,8 @@
+#En esta clase veremos la sentencia if-else
+condicion = 10
+if condicion == True:
+    print('Condición verdadera')
+elif condicion == False:
+    print('Condición falsa')
+else:
+    print('Condición sin especificar')
