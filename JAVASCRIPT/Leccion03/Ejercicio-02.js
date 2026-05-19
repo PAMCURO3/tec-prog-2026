d7bfe5bc5c49a82ee09e34a5dbd1277f28dc103f
Pamela Cuello <pam.rodriguez3@gmail.com>
2026-05-18 21:04:50 -0300

    Ejercicios Clase 7

---
+++ JAVASCRIPT/Leccion03/Ejercicio-02.js
@@ -0,0 +1,9 @@
+// Número definido directamente en el código
+let numero = 8;
+
+// Verificamos si es par o impar
+if (numero % 2 == 0) {
+    console.log("El número es par");
+} else {
+    console.log("El número es impar");
+}
