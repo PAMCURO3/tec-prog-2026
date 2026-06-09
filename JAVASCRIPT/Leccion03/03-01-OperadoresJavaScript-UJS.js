8383b53b5aa6d6889c2a5af58cfe23fb0ebb17c1
Pamela G. M. Cuello <pam.rodriguez3@gmail.com>
2026-06-09 20:13:09 -0300

    Leccion 03 JS

---
+++ JAVASCRIPT/Leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -0,0 +1,8 @@
+// Ejercicio para encontrar números pares
+let parInpar = 1;
+if(parInpar % 2 ==0){
+    console.log("Es un número PAR");
+}
+else{
+    console.log("Es un número IMPAR");
+}
--- JAVASCRIPT/Leccion04/Ejercicio-04-01.js
+++ JAVASCRIPT/Leccion04/Ejercicio-04-01.js
@@ -19,3 +19,32 @@ function getDay(n) {
         throw now error('out of range')
     }
 }
+//Ampliando el valor del uso let y const
+/*
+con var puede reasignar en cualquier momento este
+forma parte del ambito global
+un error es que sobreesciba*/
+
+var nombre = "Ariel";
+nombre = "Osvaldo";
+console.log(nombre);
+
+function saludar (){
+    var nombre = "Natalia";
+    console.log(nombre);
+}
+console.log(nombre); //Aquí no lee el dato en la función
+
+if(true){
+    var edad = 34;
+    console.log(edad)
+}
+console.log(edad); //en la función funcionó correctamente en la estructura ir falló
+
+/* 
+let: esta puede ser reasignada en cualquier momento
+la diferencia es que su ambito de bloque,
+solo dispone dentro de un bloque de llaves 
+o dentro de una función
+*/
+
