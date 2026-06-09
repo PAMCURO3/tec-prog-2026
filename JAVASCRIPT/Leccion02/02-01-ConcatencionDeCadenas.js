9ac38ace4c3101a54b95e8496c32cc397ee0f808
Pamela G. M. Cuello <pam.rodriguez3@gmail.com>
2026-06-09 19:34:34 -0300

    Clase-7-js

--- JAVASCRIPT/Leccion02/02-01-ConcatencionDeCadenas.js
+++ JAVASCRIPT/Leccion02/02-01-ConcatencionDeCadenas.js
@@ -13,3 +13,22 @@ console.log(juntos);
 
 nombre += apellido; //Terecera manera de concatenar usando el operador de simplificado
 console.log(nombre);
+
+//hoy no se usa var se utiliza let y const
+let nombre2 = "pedro";
+console.log(nombre2);
+
+const apellido2 = "Lepes";
+//apellido 2= "Peres"; una constante no puede ser modificada
+console.log(apellido2)
+
+let x, y; //se pueden crear varias variables dentro de una misma linea
+x = 17, y = 21; // se puede hacer asignacion de varias variables dentro de una misma linea
+let z = x + y; //se asigna el valor de la operacion
+console.log(z)
+
+let _1num = 31; // No utlizar números para iniciar el nombre de una variable
+let rompiendo = "rompe"; // No utilizar palabras reservadas para variables
+
+console.log(_1num)
+console.log(rompiendo)
--- JAVASCRIPT/Leccion03/Ejercicio-02.js
+++ JAVASCRIPT/Leccion03/Ejercicio-02.js
@@ -1,5 +1,6 @@
 // Número definido directamente en el código
-let numero = 8;
+//Ejercicio para encontrar números pares
+let numero = 18;
 
 // Verificamos si es par o impar
 if (numero % 2 == 0) {
@@ -7,3 +8,12 @@ if (numero % 2 == 0) {
 } else {
     console.log("El número es impar");
 }
+
+//Ejercicio: es mayor de edad
+let edad = 18, adulto = 18;
+if (edad >= adulto) {
+    console.log("Usted es una persona adulta")
+}
+else{
+    console.log("Usted es una persona menor de edad")
+}   
