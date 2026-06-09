c798f51a499fbd3085cbfc8b0a8d9065c3b74485
Pamela G. M. Cuello <pam.rodriguez3@gmail.com>
2026-06-09 20:42:25 -0300

    Clase 7 - Js video 29

--- JAVASCRIPT/Leccion04/Ejercicio-04-01.js
+++ JAVASCRIPT/Leccion04/Ejercicio-04-01.js
@@ -1,8 +1,8 @@
 /*const se utiliza para valores constantes que no
 pueden ser reasignados*/
 
-const fechaNacimiento = 2006;
-console.log(fechaNacimiento);
+const fechaNacimiento1 = 2006;
+console.log(fechaNacimiento1);
 
 /* fechaNacimiento = 2003;
 /* console.log(fechaNacimiento); // solo se ejecuta console anteior
@@ -12,12 +12,23 @@ DRY (Don't Repeat Yourself)
 */
 
 let days = [
-    'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado','Domingo'];
+    'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'
+];
 
 function getDay(n) {
     if (n < 1 || n > 7) {
-        throw now error('out of range')
+        throw new Error('out of range');
     }
+
+    return days[n - 1];
+}
+
+try {
+    console.log(getDay(1)); // Lunes
+    console.log(getDay(5)); // Viernes
+    console.log(getDay(8)); // Error
+} catch (err) {
+    console.log(err.message);
 }
 //Ampliando el valor del uso let y const
 /*
@@ -25,21 +36,25 @@ con var puede reasignar en cualquier momento este
 forma parte del ambito global
 un error es que sobreesciba*/
 
-var nombre = "Ariel";
-nombre = "Osvaldo";
-console.log(nombre);
+var nombre1 = "Ariel";
+nombre1 = "Osvaldo";
+console.log(nombre1);
+
+var nombre2 = "Natalia"
+nombre2 = "Romina"
+console.log(nombre2);
 
 function saludar (){
-    var nombre = "Natalia";
-    console.log(nombre);
+    var nombre2 = "Natalia";
+    console.log(nombre2);
 }
-console.log(nombre); //Aquí no lee el dato en la función
+console.log(nombre2); //Aquí no lee el dato en la función
 
 if(true){
-    var edad = 34;
-    console.log(edad)
+    var edad2 = 34;
+    console.log(edad2)
 }
-console.log(edad); //en la función funcionó correctamente en la estructura ir falló
+console.log(edad2); //en la función funcionó correctamente en la estructura ir falló
 
 /* 
 let: esta puede ser reasignada en cualquier momento
@@ -47,4 +62,29 @@ la diferencia es que su ambito de bloque,
 solo dispone dentro de un bloque de llaves 
 o dentro de una función
 */
+function saludar2(){
+    let nombre2 = "Ariel";
+    console.log(nombre2);
+}
+console.log (nombre2);
 
+if(true){
+    let edad2 = 33;
+    console.log(edad2);
+}
+console.log(edad2);
+
+/* 
+Const se utiliza para valores constantes 
+que no pueden ser
+reasignados
+*/
+
+const fechaNacimiento2 = 2006;
+console.log(fechaNacimiento2);
+fechaNacimiento3 = 2003;
+console.log(fechaNacimiento3); //Solo se ejecuta el console anterior
+
+var nombre4 = "Pamela"
+nombre4 = "Pamelita"
+console.log(nombre4);
