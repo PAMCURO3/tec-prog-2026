6ae3b6ba97423b8f7c4917ade1f071f75dae41c4
Pamela G. M. Cuello <pam.rodriguez3@gmail.com>
2026-06-09 19:51:08 -0300

    Clase 7 JS - 8.1

--- JAVASCRIPT/Leccion03/Ejercicio-02.js
+++ JAVASCRIPT/Leccion03/Ejercicio-02.js
@@ -17,3 +17,13 @@ if (edad >= adulto) {
 else{
     console.log("Usted es una persona menor de edad")
 }   
+
+//Ejercicio: Dentro de un rango
+let dentroRango = 5; // Aqui vamos a cambiar el valor para ver si estamos en el rango
+let valMin = 0, valMax = 10;
+if(dentroRango >= valMin && dentroRango <= valMax){
+    console.log("Está dentro del rango establecido")
+}
+else{
+    console.log("Está fuera del rango establecido")
+}
