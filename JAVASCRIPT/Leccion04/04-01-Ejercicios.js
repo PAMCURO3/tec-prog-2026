0f4fa480872889d862ea0b92ef1175abe68e99de
Pamela G. M. Cuello <pam.rodriguez3@gmail.com>
2026-06-09 23:23:36 -0300

    Video 37 JS - Hora día

--- JAVASCRIPT/Leccion04/04-01-Ejercicios.js
+++ JAVASCRIPT/Leccion04/04-01-Ejercicios.js
@@ -18,5 +18,29 @@ else{
 }
 console.log("El valor corresponde a la estacion del año: "+estacion);
 
-//Ejercicio 2 : Hora del día
-
+//Ejercicio 2 Video 37: Hora del día
+/* 
+de 6 a 11 nos saluda: Good morning
+de 12 a 16 nos saluda: Good afternoon
+de 17 a 19 nos saluda: Good evening
+de 20 a 23 nos saluda: Good night
+Trabajaremos con 24 horas
+*/
+let horaDia = 9;
+let mensaje;
+if(horaDia >= 6 && horaDia <= 11){
+	mensaje = "Good morning";
+}
+else if(horaDia >= 12 && horaDia <= 16){
+	mensaje = "Good Afternoon";
+}
+else if(horaDia >= 17 && horaDia <= 19){
+	mensaje = "Good evening";
+}
+else if(horaDia >= 20 && horaDia <= 23){
+	mensaje = "Good night";
+}
+else{
+	mensaje = "valor incorrecto";
+}
+console.log(mensaje);
