bf05e147a760025c4646d34e250801060d74b9dd
Pamela G. M. Cuello <pam.rodriguez3@gmail.com>
2026-06-11 20:20:17 -0300

    JV-014-Ejercicio Area de un rectángulo-Clase 7

--- JAVA/Leccion1/EjercicioRectangulo/src/ejerciciorectangulo/EjercicioRectangulo.java
+++ JAVA/Leccion1/EjercicioRectangulo/src/ejerciciorectangulo/EjercicioRectangulo.java
@@ -1,4 +1,4 @@
-
+//Ejercicio Sacar el área de un Rectángulo
 package ejerciciorectangulo;
 
 import java.util.Scanner;
@@ -9,17 +9,17 @@ public class EjercicioRectangulo {
 
         Scanner entrada = new Scanner(System.in);
 
-        System.out.println("Digite el alto del rectangulo: ");
+        System.out.println("Digite el alto del rectangulo en cm: ");
         int alto = Integer.parseInt(entrada.nextLine());
 
-        System.out.println("Digite el ancho del rectangulo: ");
+        System.out.println("Digite el ancho del rectangulo en cm: ");
         int ancho = Integer.parseInt(entrada.nextLine());
 
         int area = alto * ancho;
         int perimetro = (alto + ancho) * 2;
 
-        System.out.println("El area del rectangulo es: " + area);
-        System.out.println("El perimetro del rectangulo es: " + perimetro);
+        System.out.println("El area del rectangulo es: " + area + "cm2");
+        System.out.println("El perimetro del rectangulo es: " + perimetro + "cm");
 
         entrada.close();
     }
