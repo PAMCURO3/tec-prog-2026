62a705b0ffa1f8dbba05572d72833019e638a3cf
Pamela G. M. Cuello <pam.rodriguez3@gmail.com>
2026-06-11 23:03:57 -0300

    JV-023-Sentencia de Switch-Clase 9

--- JAVA/Leccion1/Leccion2/src/Leccion2/Leccion2.java
+++ JAVA/Leccion1/Leccion2/src/Leccion2/Leccion2.java
@@ -1,10 +1,12 @@
 package Leccion2;
 
+import java.util.Scanner;
+
 public class Leccion2 {
 
     public static void main(String[] args) {
-
-        var condicion = false;
+        
+        /*var condicion = false;
         if (condicion) {
             System.out.println("Condicion Verdadera");//Condicional simple
         } else {
@@ -26,6 +28,36 @@ public class Leccion2 {
         } else {
             numeroTexto = "Numero no encontrado";
         }
-            System.out.println("numeroTexto = " + numeroTexto);
+            System.out.println("numeroTexto = " + numeroTexto);*/
+        //Sentencia de control Switch
+        //var numero = 2;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite un numero del 1 al 4 ");
+        int numero = Integer.parseInt(entrada.nextLine());
+        
+        var numeroTexto = "Valor desconocido";
+        switch (numero) {
+            case 1:
+                numeroTexto = "Nummero uno";
+                break;
+            case 2:
+                numeroTexto = "Numero dos";
+                break;
+            case 3:
+                numeroTexto = "Numero tres";
+                break;
+            case 4:
+                numeroTexto = "Numero cuatro";
+                break;
+                
+            default:
+                numeroTexto = "Numero no encontrado";
         }
+        
+                System.out.println("numeroTexto = " + numeroTexto);
+                
+                entrada.close(); 
+                
+        }
+
     }
