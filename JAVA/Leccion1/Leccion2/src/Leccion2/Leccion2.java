a6b8705448832ecc1675a72c0138ac2f5b2e2ffb
Pamela G. M. Cuello <pam.rodriguez3@gmail.com>
2026-06-11 21:46:50 -0300

    JV-020-Sentencia de Control If-Else-Clase 9

---
+++ JAVA/Leccion1/Leccion2/src/Leccion2/Leccion2.java
@@ -0,0 +1,15 @@
+package Leccion2;
+
+public class Leccion2 {
+
+    public static void main(String[] args) {
+
+    var condicion = false;
+    if (condicion) {
+    System.out.println("Condicion Verdadera");//Condicional simple
+    } else {
+      System.out.println("Condicion Falsa");//condicional doble
+    }
+    }
+
+}
