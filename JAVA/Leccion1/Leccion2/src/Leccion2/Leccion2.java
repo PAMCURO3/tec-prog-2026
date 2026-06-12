cc869f079f0ac6a8d9695cb126f8d2f9bf6e6cfc
Pamela G. M. Cuello <pam.rodriguez3@gmail.com>
2026-06-11 22:22:45 -0300

    JV-021-Ejercicio If-Else Clase 9

--- JAVA/Leccion1/Leccion2/src/Leccion2/Leccion2.java
+++ JAVA/Leccion1/Leccion2/src/Leccion2/Leccion2.java
@@ -4,12 +4,28 @@ public class Leccion2 {
 
     public static void main(String[] args) {
 
-    var condicion = false;
-    if (condicion) {
-    System.out.println("Condicion Verdadera");//Condicional simple
-    } else {
-      System.out.println("Condicion Falsa");//condicional doble
-    }
-    }
+        var condicion = false;
+        if (condicion) {
+            System.out.println("Condicion Verdadera");//Condicional simple
+        } else {
+            System.out.println("Condicion Falsa");//condicional doble
+        }
 
-}
+        var numero = 4;
+
+        var numeroTexto = "Numero desconocido";
+
+        if (numero == 1) {
+            numeroTexto = "Numero uno";
+        } else if (numero == 2) {
+            numeroTexto = "Numero dos";
+        } else if (numero == 3) {
+            numeroTexto = "Numero tres";
+        } else if (numero == 4) {
+            numeroTexto = "Numero cuatro";
+        } else {
+            numeroTexto = "Numero no encontrado";
+        }
+            System.out.println("numeroTexto = " + numeroTexto);
+        }
+    }
