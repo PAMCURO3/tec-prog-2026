92e9af42267f0e5777be15a89650030931c64da5
Pamela G. M. Cuello <pam.rodriguez3@gmail.com>
2026-08-13 19:36:06 -0300

    JV-003-SS-Labels

--- SEGUNDO SEMESTRE/New Folder/CicloWhile/src/ciclowhile/CicloWhile.java
+++ SEGUNDO SEMESTRE/New Folder/CicloWhile/src/ciclowhile/CicloWhile.java
@@ -13,19 +13,23 @@ public class CicloWhile {
             System.out.println("contador = " + contador);
             contador++;
         } while (contador <= 7);
-
+        
+        //Uso de las palabras break - continue junto a las etiquetas (labels)
+        inicio: 
         for (var contando = 0; contando < 7; contando++) {
             if (contando % 2 == 0) {
                 System.out.println("contando = " + contando);
-                break;
+                break inicio;
             }
         }
+        
+        //Continue
         for (var contando = 0; contando < 7; contando++) {
              if(contando % 2 != 0){
                 continue;//vamos a la siguiente iteración 
             }
             System.out.println("contando = " + contando);
+            }
         }
-        
     }
-}
+
