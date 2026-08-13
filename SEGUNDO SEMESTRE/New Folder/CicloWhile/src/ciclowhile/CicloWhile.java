c03dbbfd1406a5cfa2c9871cab47fcd67f39d136
Pamela G. M. Cuello <pam.rodriguez3@gmail.com>
2026-08-13 18:49:52 -0300

    creacion de capeta de segundo semestre

---
+++ SEGUNDO SEMESTRE/New Folder/CicloWhile/src/ciclowhile/CicloWhile.java
@@ -0,0 +1,14 @@
+
+package ciclowhile;
+
+public class CicloWhile {
+
+    public static void main(String[] args){
+        var conteo = 0;// Inferencia de tipos
+        while(conteo <= 8 ){
+            System.out.println("conteo = " + conteo);
+            conteo++;//Vamos aumentando la variable 
+        }
+                
+    }
+}
