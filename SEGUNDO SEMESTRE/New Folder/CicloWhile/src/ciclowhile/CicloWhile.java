9e965e0f035ce4870648a890ba271528393bc83c
Pamela G. M. Cuello <pam.rodriguez3@gmail.com>
2026-08-13 19:28:11 -0300

    JV-002-SS- Break-Continue

--- SEGUNDO SEMESTRE/New Folder/CicloWhile/src/ciclowhile/CicloWhile.java
+++ SEGUNDO SEMESTRE/New Folder/CicloWhile/src/ciclowhile/CicloWhile.java
@@ -1,22 +1,31 @@
-
 package ciclowhile;
 
 public class CicloWhile {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         var conteo = 0;// Inferencia de tipos
-        while(conteo <= 8 ){
+        while (conteo <= 8) {
             System.out.println("conteo = " + conteo);
             conteo++;//Vamos aumentando la variable 
         }
         var contador = 0;
-        do{
+        do {
             System.out.println("contador = " + contador);
             contador++;
-        }while(contador <= 7);
-        
-        for(var contando = 0;contando < 7; contando++ ){
+        } while (contador <= 7);
+
+        for (var contando = 0; contando < 7; contando++) {
+            if (contando % 2 == 0) {
+                System.out.println("contando = " + contando);
+                break;
+            }
+        }
+        for (var contando = 0; contando < 7; contando++) {
+             if(contando % 2 != 0){
+                continue;//vamos a la siguiente iteración 
+            }
             System.out.println("contando = " + contando);
         }
+        
     }
 }
