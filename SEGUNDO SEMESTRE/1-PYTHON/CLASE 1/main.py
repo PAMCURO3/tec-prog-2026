af1b5d06e4eb50dfa4d5371609d039efffbfb64e
Pamela G. M. Cuello <pam.rodriguez3@gmail.com>
2026-08-26 19:54:16 -0300

    PY-002-SS-Clase 2 - Tupplas

--- SEGUNDO SEMESTRE/1-PYTHON/CLASE 1/main.py
+++ SEGUNDO SEMESTRE/1-PYTHON/CLASE 1/main.py
@@ -102,11 +102,126 @@ cocinaLista[0] = 'Plato'
 cocina = tuple(cocinaLista) #conversion de lista a tupla
 print('\n', cocina)
 
-
 #Esto es para eliminar la tupla
 #del cocina
 #print(cocina)
 
+# CLASE 2
+# Tipo set
+planetas = {"Marte", "Júpiter", "Venus"}
+print(len(planetas)) # Usamos la función len = length significa largo
 
+# Revisar si un elemento existe dentro de set
+print("Júpiter" in planetas)
 
+# Agregar un elemento
+planetas.add("Tierra") # add es una función
+print(planetas)
 
+# Eliminar elementos, puede arrojar un error si el elemento no exite
+planetas.remove("Júpiter") # Esta función ante un mal ingreso u inexistencia del elemento da error
+print(planetas)
+planetas.discard("Tierra") # Esta función no nos presenta ningún tipo de error
+print(planetas)
+
+# Limpiar set o conjunto
+planetas.clear()
+print(planetas)
+
+#Eliminar set
+del planetas
+# print(planetas) # al eliminar nos muestra un error
+
+# "Maradona":10 Un diccionario está compuesto por dos elementos
+# UNA LLAVE Y UN VALOR
+# dict(key,value)
+diccionario = {
+    "IDE":"Integrated Development Environment",
+    "POO":"Programación Orientada a Objetos",
+    "SABD":"Sistema de Administración de Base de Datos",
+}
+# Verificar la cantidad de elementos del diccionario
+print(len(diccionario))
+print(diccionario)
+
+# Acceder a un diccionario con la llave (key)
+print(diccionario["IDE"])
+
+# Otra forma de recuperar un elemento
+print(diccionario.get("POO"))
+print(diccionario.get("SABD"))
+
+# Modificamos los elementos
+diccionario["IDE"] = "Entorno de Desarrollo Integrado"
+print(diccionario)
+
+# Cómo recorrer los elementos
+for termino in diccionario: # Recorremos mostrando solo las llaves
+    print(termino)
+
+# Necesitamos una función para recorer un diccionario
+for termino, valor in diccionario.items():
+    print(termino, valor)
+
+# Otras maneras de acceder a un diccionario
+for termino in diccionario.keys(): # Estamos usando una función
+    print(termino) # Muestra solo las llaves
+
+for valor in diccionario.values(): # Usamos una función para acceder al valor
+    print(valor)
+
+# Comprobar la existencia de algún elemento
+print("IDE" in diccionario) # devuelve un booleano
+
+# Agregar un elemento
+diccionario["PK"] = "Primary key"
+print(diccionario)
+
+# Eliminar un elemento
+diccionario.pop("SABD")
+print(diccionario)
+
+# Vaciar un diccionario
+diccionario.clear()
+print(diccionario)
+
+# Eliminar diccionario
+# del diccionario # El diccionario se borró
+print(diccionario)
+
+# Concatenamos listas
+lista1 = [1, 2, 3, 1]
+lista2 = [4, 5, 6, 1]
+lista3 = lista1+lista2 # Concatenación
+print(lista3)
+
+lista3.extend([7, 8, 9, 1]) # Función para agregar varios elementos a una lista
+print(lista3)
+
+print(lista3.index(5)) # Función para ubicar en que indice está el valor ingresado
+# print(lista3.index(0)) # Esto daría un error por no ser el elemento parte de la lista
+
+# Cómo saber cuántos valores repitos hay en una lista
+print(lista3.count(1)) # Cuenta cuántos valores iguales hay dentro de la lista
+
+# Para poner al reves una lista
+lista3.reverse()
+print(lista3)
+
+# Para que una lista se multiplique repitiendo sus elementos
+lista3 = lista3 * 2
+print(lista3)
+
+# Métodos de ordenamiento, en python es una función
+lista3.sort() # Ordena los elemntos ascendentemente
+print(lista3)
+lista3.sort(reverse=True) # Ordena descendentemente
+print(lista3)
+
+# Repaso de Tuplas
+tupla = (4, "Hola", 6.78, [1, 2, 78], 4, "Hola") # Puede tener diferentes tipos de datos dentro
+print(tupla)
+
+print(4 in tupla) # Acción booleana, su respuesta es de tipo booleana
+# Lo que podemos usar dentro de tupplas son: index, count, len
+# En tuplas se puede convertir de tupla a lista y de lista a tupla
